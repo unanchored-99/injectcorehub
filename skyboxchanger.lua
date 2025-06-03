d0c476de1e521ff274088f0b5f9cbe8c4dfa9877
unanchored-99 <incoghax@gmail.com>
2025-06-03 15:57:38 -0400

    Create skyboxchanger.lua

---
+++ skyboxchanger.lua
@@ -0,0 +1,12 @@
+decal = "rbxassetid://"
+
+SkyBox = Instarnce.new("Sky")
+SkyBox.Name = "Plus"
+SkyBox.Parent = game.Lighting
+SkyBox.SkyboxBk = decal
+SkyBox.SkyboxDn = decal
+SkyBox.SkyboxFt = decal
+SkyBox.SkyboxRt = decal
+SkyBox.SkyboxLf = decal
+SkyBox.SkyboxUp = decal
+SkyBox.StarCount = 0
